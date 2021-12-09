4875729da5e290919bba4b25e23668478435ffa8
cyw <18604469853@163.com>
2021-12-09 10:09:18 +0800

    vuex init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from "vue";
 import App from "./App";
 import uView from "uview-ui";
+import store from './store';
 
 Vue.config.productionTip = false;
 
 App.mpType = "app";
 Vue.use(uView);
 
+Vue.prototype.$store = store;
+
 const app = new Vue({
+  store,
   ...App,
 });
 app.$mount();
